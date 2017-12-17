6269b77927fc554707d7a88a64205fba47c0616d
Marta Motyka <32452829+marrtam@users.noreply.github.com>
2017-12-17 19:30:30 +0100

    Update TestEmployee.java
    test employee

--- test/hotelapp/TestEmployee.java
+++ test/hotelapp/TestEmployee.java
@@ -1,4 +1,6 @@
 package hotelapp;
+import java.time.LocalDate;
+import org.junit.Assert;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -14,5 +16,12 @@ public class TestEmployee {
 
     public TestEmployee() {
     }
+     public void testSetPrivileges(){
+        Employee employee = new Employee(1, "Jan", "Kowalski", 
+                LocalDate.parse("1984-05-20"), "janko", "876");
+        Assert.assertEquals(Privileges.ADMIN, employee.getPrivileges());
+    }
+    
+    
     
 }
