364c83137c64fa4307cdfe572598a60151e8656d
kiper3k <kiper3k@gmail.com>
2017-12-17 17:34:45 +0100

    Edited TestClient and added TestUser class.

--- test/hotelapp/TestClient.java
+++ test/hotelapp/TestClient.java
@@ -1,5 +1,8 @@
 package hotelapp;
 
+import java.time.LocalDate;
+import org.junit.Assert;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,5 +14,17 @@ package hotelapp;
  * @author kiper
  */
 public class TestClient {
+    
+    public void testSetPrivileges(){
+        Client client = new Client(2, "Monika", "Cabaj", 
+                LocalDate.parse("1987-03-14"), "mc", "000", 0);
+        Assert.assertEquals(Privileges.CLIENT, client.getPrivileges());
+    }
+    
+    public void testSetNbOfReservations(){
+        Client client = new Client(2, "Monika", "Cabaj", 
+                LocalDate.parse("1987-03-14"), "mc", "000", 0);
+        Assert.assertEquals(0, client.getNbOfReservations());
+    }
     
 }
